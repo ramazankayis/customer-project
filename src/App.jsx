a5ebb8e434a4ce119ab84e1f1e02810ca298c4f5
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2023-10-20 15:03:40 +0300

    project finish

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import CustomerForm from "./components/CustomerForm/CustomerForm";
+import CustomerList from "./components/CustomerList/CustomerList";
+import styled from "styled-components";
+
+const AppWraper = styled.div`
+  padding: 30px;
+  background-color: white;
+  border-radius: 10px;
+
+  & h1 {
+    margin-bottom: 20px;
+    @media (max-width: 576px) {
+      font-size: 20px;
+    }
+  }
+`;
+function App() {
+  const [customers, setCustomers] = useState([]);
+
+  const addNewCustomer = (newCustomer) => {
+    // setCustomers([...customers, newCustomer]);
+    setCustomers((prevState) => [newCustomer, ...prevState]);
+  };
+
+  return (
+    <AppWraper className="App">
+      <h1>Customer Manage System</h1>
+      <CustomerForm addNewCustomer={addNewCustomer} />
+      {customers.length === 0 && "There are no customers..."}
+      <CustomerList customers={customers} setCustomers={setCustomers} />
+    </AppWraper>
+  );
+}
+
+export default App;
